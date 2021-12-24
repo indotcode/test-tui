96cb67c88cb7fc970f7cbb3d5253b2189567495a
indotcode <oruno999@gmail.com>
2021-12-24 16:31:17 +0700

    Поправлены стандартные тесты авторизации и регистрации

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Auth;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
@@ -20,24 +21,33 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen()
     {
-        $user = User::factory()->create();
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@email.ru',
+            'password' => Hash::make('123456'),
+            'id_role' => 1
+        ]);
 
         $response = $this->post('/login', [
             'email' => $user->email,
-            'password' => 'password',
+            'password' => Hash::make('123123'),
         ]);
 
-        $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
 
     public function test_users_can_not_authenticate_with_invalid_password()
     {
-        $user = User::factory()->create();
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@email.ru',
+            'password' => Hash::make('123456'),
+            'id_role' => 1
+        ]);
 
         $this->post('/login', [
             'email' => $user->email,
-            'password' => 'wrong-password',
+            'password' => Hash::make('123456'),
         ]);
 
         $this->assertGuest();
